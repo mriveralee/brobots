dc446aade64f637d27abc99750f3f0de90837fde
Michael Rivera <mrivera.lee@gmail.com>
2013-02-17 00:25:36 -0500

    Updated to send to brobot

--- app.js
+++ app.js
@@ -37,21 +37,21 @@ app.configure('development', function(){
 app.get('/driver', function(req, res) {
   var driverID = req.param('driver_id') ? req.param('driver_id') : "" + parseInt(Math.round(Math.random()*103843)+7);
   console.log("Driver ID: " + driverID);
-  res.render('index', {PAGE_TITLE: "Brobots - Driver"});
+  res.render('index', {USER_TYPE: "Brobots - Driver"});
 });
 
 
 app.get('/gripper', function(req, res) {
   var gripperID = req.param('gripper_id') ? req.param('gripper_id') : "" + parseInt(Math.round(Math.random()*103843)+17);
   console.log("Gripper ID: " + gripperID);
-  res.render('index', {PAGE_TITLE: "Brobots - Gripper"});
+  res.render('index', {USER_TYPE: "Gripper"});
 });
 
 
 app.get('/brobot', function(req, res) {
   var botID = req.param('bot_id') ? req.param('bot_id') : "" + parseInt(Math.round(Math.random()*103843)+334);
   console.log("Bot ID: " + botID);
-  res.render('index', {PAGE_TITLE: "BroBots - Bot"});
+  res.render('index', {USER_TYPE: "Bot"});
 });
 
 
@@ -62,18 +62,18 @@ io.sockets.on('connection', function (socket) {
   
   function updatedBotData(botData) {
     if (botData) {
-      console.log("Emitting received bot data");
+      //console.log("Emitting received bot data");
       socket.broadcast.emit('updated-bot-data', botData);
     }
   }
  
   socket.on('updated-drive-data', function (driveData) {
-    console.log("Drive Data: " + driveData);
+    //console.log("Drive Data: " + driveData);
     updatedBotData(driveData);
   });
 
   socket.on('updated-grip-data', function(gripData) {
-    console.log("grip Data: " + gripData);
+    //console.log("grip Data: " + gripData);
     updatedBotData(gripData);
   });
 
